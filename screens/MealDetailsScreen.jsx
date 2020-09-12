3bb1e41343f4128f983c1c0bbf223aa65ba57aba
mubeen <mubeenarman19@gmail.com>
2020-09-12 22:19:41 +0900

    add header in meal details

--- screens/MealDetailsScreen.jsx
+++ screens/MealDetailsScreen.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
 
 const MealDetailScreen = (props) => {
     return (
@@ -7,6 +8,14 @@ const MealDetailScreen = (props) => {
             <Text>This is meal detail screen</Text>
         </View>
     );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+    const mealId = navigationData.navigation.getParam("mealId");
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+    return {
+        headerTitle: selectedMeal.title,
+    };
 };
 
 const styles = StyleSheet.create({
