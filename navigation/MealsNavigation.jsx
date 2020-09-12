d78eaa40135336eefaa84d9cc65165682b266293
mubeen <mubeenarman19@gmail.com>
2020-09-13 01:16:10 +0900

    add material tab tar for android

--- navigation/MealsNavigation.jsx
+++ navigation/MealsNavigation.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import CategoriesScreen from "../screens/CategoriesScreen";
@@ -8,6 +9,7 @@ import Color from "../constants/Color";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import FavouriteScreem from "../screens/FavouriteScreen";
 import { Ionicons } from "@expo/vector-icons";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 
 const MealsNavigation = createStackNavigator(
     {
@@ -27,41 +29,47 @@ const MealsNavigation = createStackNavigator(
     }
 );
 
-const MealFavTabNavigation = createBottomTabNavigator(
-    {
-        Meals: {
-            screen: MealsNavigation,
-            navigationOptions: {
-                tabBarIcon: (tabInfo) => {
-                    return (
-                        <Ionicons
-                            name='ios-restaurant'
-                            size={25}
-                            color={tabInfo.tintColor}
-                        />
-                    );
-                },
+const tabScreenConfig = {
+    Meals: {
+        screen: MealsNavigation,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name='ios-restaurant'
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+                );
             },
-        },
-        Favourites: {
-            screen: FavouriteScreem,
-            navigationOptions: {
-                tabBarIcon: (tabInfo) => {
-                    return (
-                        <Ionicons
-                            name='ios-star'
-                            size={25}
-                            color={tabInfo.tintColor}
-                        />
-                    );
-                },
-            },
+            tabBarColor: Color.primaryColor,
         },
     },
-    {
-        tabBarOptions: {
-            activeTintColor: Color.accenColor,
+    Favourites: {
+        screen: FavouriteScreem,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name='ios-star'
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+                );
+            },
+            tabBarColor: Color.accenColor,
         },
-    }
-);
+    },
+};
+const MealFavTabNavigation =
+    Platform.OS === "android"
+        ? createMaterialBottomTabNavigator(tabScreenConfig, {
+              activeColor: "white",
+              shifting: true,
+          })
+        : createBottomTabNavigator(tabScreenConfig, {
+              tabBarOptions: {
+                  activeTintColor: Color.accenColor,
+              },
+          });
 export default createAppContainer(MealFavTabNavigation);
